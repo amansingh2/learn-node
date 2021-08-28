a3f3a231d74f2d3f64ed2b734713951f5b44c458
Aman <aman.nitmizoram@gmail.com>
2021-08-28 23:09:54 +0530

    Dynamic content banana sikha mai

--- app.js
+++ app.js
@@ -23,13 +23,28 @@
 // app.use('/',router)
 // app.listen(4500)
 
+// const express = require('express')
+// const app = express()
+// app.set('view engine','ejs')
+
+// app.get("/pro",function(req,res){
+//   // res.sendFile(__dirname+'/Home.html')
+//   res.render('Profile')
+// })
+
+// app.listen(4500)
+
 const express = require('express')
 const app = express()
 app.set('view engine','ejs')
 
-app.get("/pro",function(req,res){
+app.get("/profile/:name",function(req,res){
   // res.sendFile(__dirname+'/Home.html')
-  res.render('Profile')
+  // res.render('Profile')
+  data = {email:'aman.nitmizoram@gmail.com',address:'noida',skills:['node js' ,'React' , 'html']}
+  res.render('Profile',{name:req.params.name,data:data})
 })
 
 app.listen(4500)
+
+
